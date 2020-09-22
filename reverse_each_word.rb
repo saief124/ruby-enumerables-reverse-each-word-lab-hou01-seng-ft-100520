88fad3bd653c6b66d98a525952efa4395fe2a02e
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 23:51:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@ c=""
 b=str.split(" ") #split the string and return the words in an array
 #i=0
 b.each do |e|#while i<b.length do
-  a[e]=b[e].reverse #uses array method reverse to reverse the words
+  a[]=b[e].reverse #uses array method reverse to reverse the words
 #i +=1
 end
 
