33fb56e6c2ac5d7e9afc4d3864819425dcbab4d1
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 23:44:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,15 +3,14 @@ def reverse_each_word(str)
 b=[]
 a=[]
 c=""
-b=str.split(" ")
-i=0
-while i<b.length do
-  a[i]=b[i].reverse
-i +=1
+b=str.split(" ") #split the string and return the words in an array
+#i=0
+b.each do |i|#while i<b.length do 
+  a[i]=b[i].reverse #uses array method reverse to reverse the words
+#i +=1
 end
-# c=a.to_join(" ")
 
-c=a.join(" ")
+c=a.join(" ") #joins the array back into a string
 
 
 end #Ends the method
